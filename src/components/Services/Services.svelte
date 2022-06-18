91f61f060ea8441a20b3f32fc9b000ec16167302
milinches <ibrogenius5165@gmail.com>
2022-06-18 22:04:09 +0100

    made changes

--- src/components/Services/Services.svelte
+++ src/components/Services/Services.svelte
@@ -1,3 +1,8 @@
+<script>
+    let items = ['Wash and Fold', 'Dry Cleaning'];
+    let activeItem = 'Wash and Fold';
+</script>
+
 <div class="flex justify-center items-center bg-slate-50">
     <div class="flex justify-center md:mt-15 mt-20 mb-10 items-center w-10/12">
         <div class="justify-center items-center flex flex-col">
@@ -13,6 +18,14 @@
             <p class="text-center p-10">
                 From everyday laundry to professional dry cleaning, we have a service that will meet your needs. <br> Save 3-4 hours a week and let the experts handle it!
             </p>
+
+            <ul class="flex justify-center">
+                {#each items as item}
+                    <li class:active={item === activeItem} class="px-5 py-3">
+                        {item}
+                    </li>
+                {/each}
+            </ul>
         </div>
     </div>
 </div>
@@ -20,5 +33,10 @@
 <style>
     .h__one {
         line-height: 4rem;
+    }
+
+    .active {
+        color: #d91b42;
+        border-bottom: 2px solid #d91b42;
     }
 </style>
--- src/routes/services-pricing.svelte
+++ src/routes/services-pricing.svelte
@@ -1,6 +1,11 @@
 <script>
     import Services from "../components/Services/Services.svelte";
+    import Footer from "../components/Common/Footer.svelte";
 </script>
+
+<svelte:head>
+    <title>Call and Clean | Services and Pricing</title>
+</svelte:head>
 
 
 <Services />
